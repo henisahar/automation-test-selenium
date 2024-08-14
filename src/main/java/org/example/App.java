86f5764c067988dbe278deb338bd47a353c01f83
henisahar <saharheni6@gmail.com>
2024-08-14 16:47:45 +0100

    removing not used imports to make the code clear

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,7 @@
 package org.example;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Hello world!
@@ -8,12 +9,10 @@ package org.example;
  */
 public class App
 {
-
-
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main( String[] args )
     {
-
-        System.out.println("Hello World!");
+        logger.info("Hello World!");
     }
 }
